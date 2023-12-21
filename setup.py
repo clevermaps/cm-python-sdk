a6bdcc66b1a43ef919db78bf2be23f0083c88236
Karel Psota <karel.psota@cleveranalytics.com>
2023-12-21 10:00:07 +0100

    setup pydantic

--- setup.py
+++ setup.py
@@ -8,7 +8,8 @@ setuptools.setup(
     description="Python SDK client for the CleverMaps API",
     packages=setuptools.find_packages(),
     install_requires=[
-        'requests>=2.30.0'
+        'requests>=2.30.0',
+        'pydantic>=2.0.0'
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
