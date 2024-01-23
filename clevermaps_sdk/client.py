59eead60b115a9072dd68109c40005ce62a03de3
Karel Psota <karel.psota@cleveranalytics.com>
2024-01-23 12:26:59 +0100

    retry fix

--- clevermaps_sdk/client.py
+++ clevermaps_sdk/client.py
@@ -5,19 +5,17 @@ from tenacity import retry, stop_after_attempt, wait_fixed, retry_if_result
 
 from .exceptions import AccessTokenException
 
-RETRY_COUNT = 0
-RETRY_WAIT = 0
 
 class Client:
 
     def __init__(self, access_token, server_url, retry_count, retry_wait):
 
         self.base_url = server_url
+        self.session = requests.Session()
+        self.retry_count = retry_count
+        self.retry_wait = retry_wait
         self.bearer_token = self._get_token(access_token)
-
-        RETRY_COUNT = retry_count
-        RETRY_WAIT = retry_wait
-
+        
     def _get_token(self, access_token):
 
         params = {
@@ -41,21 +39,29 @@ class Client:
         return resp.json()['access_token']
     
 
-    def retry_if_result_is_code(result):
+    def retry_if_result_is_code(self, result):
         
         retry_on_codes = [404]
 
-        return result.status_code in retry_on_codes
+        retry = result.status_code in retry_on_codes
+
+        return retry
     
 
-    @retry(
-        stop=stop_after_attempt(RETRY_COUNT),
-        wait=wait_fixed(RETRY_WAIT),
-        retry=retry_if_result(retry_if_result_is_code)
-    )
-    def http_request(self, method, url, params, headers):
+    def http_request_with_retry(self, method, url, params, headers):
 
-        session = requests.Session()
+        http_retry = retry(
+            stop=stop_after_attempt(self.retry_count),
+            wait=wait_fixed(self.retry_wait),
+            retry=retry_if_result(self.retry_if_result_is_code)
+        )
+
+        http_request_retry = http_retry(self.http_request)
+
+        return http_request_retry(method, url, params, headers)
+
+
+    def http_request(self, method, url, params, headers):
 
         if not headers:
             headers = {
@@ -68,27 +74,26 @@ class Client:
         url='{}{}'.format(self.base_url, url) if not bool(urlparse(url).netloc) else url
 
         if method == 'post':
-            resp = session.post(url, data=json.dumps(params), headers=headers)
+            resp = self.session.post(url, data=json.dumps(params), headers=headers)
         elif method == 'get':
-            resp = session.get(url, params=params, headers=headers)
+            resp = self.session.get(url, params=params, headers=headers)
         elif method == 'put':
-            resp = session.put(url, data=params, headers=headers)
-
-        #resp.raise_for_status()
+            resp = self.session.put(url, data=params, headers=headers)
 
         return resp
     
 
     def paginate(self, method, url, params, headers):
 
-        first_page = self.http_request(method=method, url=url, params=params, headers=headers)
+        first_page = self.http_request_with_retry(method=method, url=url, params=params, headers=headers)
+        
         first_page.raise_for_status()
         yield first_page
 
         links = first_page.json()['links']
         while [l for l in links if l['rel'] == 'next']:
             next_url = next(l['href'] for l in links if l['rel'] == 'next')
-            next_page = self.http_request(method=method, url=next_url, params={}, headers=headers)
+            next_page = self.http_request_with_retry(method=method, url=next_url, params={}, headers=headers)
             next_page.raise_for_status()
             links = next_page.json()['links']
             yield next_page
@@ -103,7 +108,7 @@ class Client:
 
     def make_request(self, method, url, params={}, headers={}):
 
-        resp = self.http_request(method=method, url=url, params=params, headers=headers)
+        resp = self.http_request_with_retry(method=method, url=url, params=params, headers=headers)
 
         resp.raise_for_status()
 
